b00f02d825484be81d29af96b1c770fad21e24b2
Denis <denisvp06@mail.ru>
2025-12-03 13:43:50 +0300

    feat: Practice 24 fix some bugs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/technology_tracker/',
+	base: './',
 	plugins: [react()],
 	resolve: {
 		alias: {
